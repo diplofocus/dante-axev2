c273c4d42aa5c9a272c2dd95e3bd9b6e9564b97a
Petar Saulic <petar.saulic@bloxico.com>
2021-01-26 16:46:26 +0100

    Update server config.

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://test.danteaxe.com',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'e47c6db6e0ba7af81d444143334523aa'),
