f8b792c32eb975933c64fb49ec5c7e419cc56b2a
Petar Saulic <petar.saulic@bloxico.com>
2021-02-26 19:12:33 +0100

    WIP

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -11,8 +11,9 @@ const MyApp = ({ Component, pageProps }) => {
   const [cart, setCart] = useLocalStorage([], "shopping_cart");
   const addItem = (item) => setCart((cartItems) => [...cartItems, item]);
   const removeItem = (id) => setCart(cart.filter((i) => i.id !== id));
+  const clearCart = () => setCart([]);
   return (
-    <Provider value={{ cart, addItem, removeItem }}>
+    <Provider value={{ cart, addItem, removeItem, clearCart }}>
       <Layout categories={pageProps.categories}>
         <ThemeProvider defaultTheme="dark">
           <Component {...pageProps} />
@@ -26,7 +27,7 @@ const MyApp = ({ Component, pageProps }) => {
 // have getStaticProps. So [[...slug]] pages still get SSG.
 // Hopefully we can replace this with getStaticProps once this issue is fixed:
 // https://github.com/vercel/next.js/discussions/10949
-MyApp.getInitialProps = async (ctx) => {
+MyApp.getServerSideProps = async (ctx) => {
   // Calls page's `getInitialProps` and fills `appProps.pageProps`
   const appProps = await App.getInitialProps(ctx);
   // Fetch global site settings from Strapi
--- frontend/pages/shoppingCart.js
+++ frontend/pages/shoppingCart.js
@@ -1,47 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { PayPalButton } from "react-paypal-button-v2";
 import { Consumer } from "../contexts/shoppingCartContext";
 import CartItem from "../components/CartItem";
 
-const ShoppingCart = () => (
-  <Consumer>
-    {({ cart, removeItem }) => (
-      <div>
-        <h1 className="prose prose-xl font-bold mt-2 mb-2">Shopping Cart</h1>
-        {cart.map((item) => (
-          <CartItem item={item} onRemove={removeItem} key={item.id} />
-        ))}
-        {cart.length ? (
-          typeof window !== "undefined" && (
-            <div className="flex p-2 float-right">
+const ShoppingCart = () => {
+  const [purchaseMade, setPurchaseMade] = useState(false);
+  return (
+    <Consumer>
+      {({ cart, removeItem, clearCart }) => (
+        <div>
+          <h1 className="prose prose-xl font-bold mt-2 mb-2">Shopping Cart</h1>
+          {cart.map((item) => (
+            <CartItem item={item} onRemove={removeItem} key={item.id} />
+          ))}
+          {cart.length ? (
+            typeof window !== "undefined" && (
               <PayPalButton
                 currency="EUR"
                 options={{
-                  clientId: "sb",
+                  clientId: process.env.NEXT_PUBLIC_PAYPAL_ID,
                   currency: "EUR",
                 }}
                 createOrder={(data, actions) => {
                   console.log(data);
                   console.log(actions);
+                  console.log(cart);
                   return actions.order.create({
-                    purchase_units: cart.map((i) => ({
-                      reference_id: i.id,
-                      amount: {
-                        currency_code: "EUR",
-                        value: "15.00",
-                      },
-                    })),
+                    purchase_units: cart.map((i) => {
+                      console.log(i);
+                      let id = i.id;
+                      if (i.finish) {
+                        id += i.finish.id;
+                      }
+                      return {
+                        reference_id: id,
+                        amount: {
+                          currency_code: "EUR",
+                          value: i.price,
+                        },
+                      };
+                    }),
                   });
                 }}
+                onApprove={(data, actions) => {
+                  console.log("completed", data, actions);
+                  clearCart();
+                  setPurchaseMade(true);
+                }}
               />
-            </div>
-          )
-        ) : (
-          <p className="prose prose-m">Your shopping cart is empty</p>
-        )}
-      </div>
-    )}
-  </Consumer>
-);
+            )
+          ) : purchaseMade ? (
+            <div>Congratulations! Your purchase was successful.</div>
+          ) : (
+            <p className="prose prose-m">Your shopping cart is empty</p>
+          )}
+        </div>
+      )}
+    </Consumer>
+  );
+};
 
 export default ShoppingCart;
