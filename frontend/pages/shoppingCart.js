9d62010aeb4c6a2cd83ed9330005e0d54fb96224
Petar Saulic <petar.saulic@bloxico.com>
2021-03-06 21:43:41 +0100

    Cart fixes.

--- frontend/pages/shoppingCart.js
+++ frontend/pages/shoppingCart.js
@@ -17,56 +17,56 @@ const ShoppingCart = () => {
               <CartItem item={item} onRemove={removeItem} key={item.id + idx} />
             ))}
           </div>
-          {cart.length || purchaseMade ? (
-            typeof window !== "undefined" && (
-              <PayPalButton
-                currency="EUR"
-                options={{
-                  clientId: process.env.NEXT_PUBLIC_PAYPAL_ID || "sb",
-                  currency: "EUR",
-                }}
-                createOrder={(data, actions) => {
-                  const payload = {
-                    purchase_units: cart.map((i) => {
-                      let desc = i.title;
-                      if (i.finish) {
-                        desc += " box with " + i.finish.Name + " finish";
-                        if (i.customDescription) {
-                          desc += ` with custom description "${i.customDescription}"`;
-                        }
-                      } else if (i.size) {
-                        desc += ` shirt size ${i.size} color ${i.color} sex ${i.sex}`;
-                      }
-                      return {
-                        reference_id: i.title,
-                        description: desc,
-                        amount: {
-                          currency_code: "EUR",
-                          value: i.price,
-                        },
-                      };
-                    }),
-                  };
-                  return actions.order.create(payload);
-                }}
-                onApprove={(data, actions) => {
-                  clearCart();
-                  const test = actions.order.get();
-                  setPurchaseMade(true);
-                  const orderResponse = {
-                    orderId: data.orderID,
-                    payerId: data.payerID,
-                    cart,
-                  };
-                }}
-              />
-            )
-          ) : purchaseMade ? (
+          {purchaseMade && (
             <div>Congratulations! Your purchase was successful.</div>
-          ) : (
+          )}
+          {!cart.length && (
             <p className="prose prose-m text-gray-300">
               Your shopping cart is empty
             </p>
+          )}
+          {typeof window !== "undefined" && (
+            <PayPalButton
+              currency="EUR"
+              options={{
+                clientId: process.env.NEXT_PUBLIC_PAYPAL_ID || "sb",
+                currency: "EUR",
+              }}
+              createOrder={(data, actions) => {
+                const payload = {
+                  purchase_units: cart.map((i) => {
+                    let desc = i.title;
+                    if (i.finish) {
+                      desc += " box with " + i.finish.Name + " finish";
+                      if (i.customDescription) {
+                        desc += ` with custom description "${i.customDescription}"`;
+                      }
+                    } else if (i.size) {
+                      desc += ` shirt size ${i.size} color ${i.color} sex ${i.sex}`;
+                    }
+                    return {
+                      reference_id: i.title,
+                      description: desc,
+                      amount: {
+                        currency_code: "EUR",
+                        value: i.price,
+                      },
+                    };
+                  }),
+                };
+                return !!cart.length && actions.order.create(payload);
+              }}
+              onApprove={(data, actions) => {
+                clearCart();
+                /* const test = actions.order.get(); */
+                setPurchaseMade(true);
+                /* const orderResponse = { */
+                /*   orderId: data.orderID, */
+                /*   payerId: data.payerID, */
+                /*   cart, */
+                /* }; */
+              }}
+            />
           )}
         </div>
       )}
